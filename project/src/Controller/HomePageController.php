1519eac493356ec70263902e49f5f3270ea9facb
novapc74 <novapc@ukr.net>
2023-09-16 17:54:28 +0300

    add review entity

--- project/src/Controller/HomePageController.php
+++ project/src/Controller/HomePageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\BranchRepository;
+use App\Repository\ReviewRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(BranchRepository $branchRepository): Response
+    public function index(BranchRepository $branchRepository, ReviewRepository $reviewRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
             'branches' => $branchRepository->findBy([], [], 6),
+            'reviews' => $reviewRepository->findBy([], [], 6),
         ]);
     }
 }
---
+++ project/src/DataFixtures/ReviewFixtures.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Review;
+use App\Entity\SocialNetwork;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+;
+
+class ReviewFixtures extends BaseFixture implements FixtureGroupInterface
+{
+    private const REVIEW_DATA = [
+        [
+            'preview' => 'Компания "Сити Сервис" - мой верный партнер в поддержании безупречного порядка. Они делают свою работу безупречно и ответственно',
+            'author' => ['Иван Петров', 'Генеральный директор компании "Акрон"'],
+        ],
+        [
+            'preview' => 'Компания "Сити Сервис" - мой верный партнер в поддержании безупречного порядка. Они делают свою работу безупречно и ответственно',
+            'author' => ['Петр Иванов', 'Главный бухгалтер компании "Озон"'],
+        ],        [
+            'preview' => 'Компания "Сити Сервис" - мой верный партнер в поддержании безупречного порядка. Они делают свою работу безупречно и ответственно',
+            'author' => ['Елена Александровна', 'Руководитель отдела продаж компании "Яндех"'],
+        ],        [
+            'preview' => 'Компания "Сити Сервис" - мой верный партнер в поддержании безупречного порядка. Они делают свою работу безупречно и ответственно',
+            'author' => ['Александра Фёдоровна', 'Заместитель директора компании "Автоваз"'],
+        ],
+        [
+            'preview' => 'Компания "Сити Сервис" - мой верный партнер в поддержании безупречного порядка. Они делают свою работу безупречно и ответственно',
+            'author' => ['Карина Евгеньевна', 'Генеральный директор компании "Сбер Банк"'],
+        ],
+        [
+            'preview' => 'Компания "Сити Сервис" - мой верный партнер в поддержании безупречного порядка. Они делают свою работу безупречно и ответственно',
+            'author' => ['Тимур Валентинович', 'Начальник охраны компании "Почта России"'],
+        ],
+
+    ];
+
+    public function loadData(ObjectManager $manager): void
+    {
+        $this->createEntity(Review::class, 6, function (Review $review, $count) {
+            $review
+                ->setPreview(self::REVIEW_DATA[$count]['preview'])
+                ->setAuthor(self::REVIEW_DATA[$count]['author']);
+        });
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return [
+            BranchFixtures::class
+        ];
+    }
+}
