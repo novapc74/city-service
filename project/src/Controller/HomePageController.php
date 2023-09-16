04746d1ef3a26d2f5cd74a40e154e4f3b9dc5770
novapc74 <novapc@ukr.net>
2023-09-16 11:16:54 +0300

    add branches to home page

--- project/src/Controller/HomePageController.php
+++ project/src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BranchRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(BranchRepository $branchRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'branches' => $branchRepository->findBy([], [], 6),
         ]);
     }
 }
