9201b5d8068c77e2db0135357ce093ac4effa59a
kosatos <kosatosfront@yandex.ru>
2023-09-24 02:14:58 +0300

    add header + footer

---
+++ project/assets/app/src/js/base/dropdowns.js
@@ -0,0 +1,32 @@
+import {addClass, removeClass} from "../utils/classMethods";
+
+export default function dropdowns() {
+
+    // const dropdowns = [...document.querySelectorAll('.base-dropdown')]
+    // dropdowns.length && dropdowns.forEach(item => {
+    //     const content = item.querySelector('.base-dropdown__inner'),
+    //         contentHeight = item.querySelector('.base-dropdown__content').offsetHeight
+    //     content.style = `--height:${contentHeight}px`
+    // })
+
+    document.addEventListener('click', (evt) => {
+        const target = evt.target.closest('[data-open-dropdown]')
+        if (target) {
+            const dropdown = target.closest(`[data-dropdown="${target.dataset.name}"]`)
+
+            if (dropdown.classList.contains('active')) {
+                removeClass(dropdown, 'active')
+                if (target.dataset.openText) {
+                    target.textContent = target.dataset.openText
+                }
+            } else {
+                addClass(dropdown, 'active')
+                if (target.dataset.hideText) {
+                    target.textContent = target.dataset.hideText
+                }
+            }
+
+        }
+    })
+}
+
---
+++ project/assets/app/src/js/base/index.js
@@ -0,0 +1,6 @@
+import dropdowns from "./dropdowns";
+import mobileMenu from "./mobileMenu";
+document.addEventListener('DOMContentLoaded', () => {
+    dropdowns()
+    mobileMenu()
+})
