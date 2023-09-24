a019ce4bd69df5dbd659c108bee148dd6da8d73c
novapc74 <novapc@ukr.net>
2023-09-24 14:02:51 +0300

    make custom truncate for fixtures

--- project/src/Repository/ProductRepository.php
+++ project/src/Repository/ProductRepository.php
@@ -21,20 +21,18 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-//    /**
-//     * @return Product[] Returns an array of Product objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * @return Product[] Returns an array of Product objects
+     */
+    public function findByParentProduct(): array
+    {
+        return $this->createQueryBuilder('p')
+            ->leftJoin('p.products', 'product')
+            ->where('product.product is null')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 
 //    public function findOneBySomeField($value): ?Product
 //    {
