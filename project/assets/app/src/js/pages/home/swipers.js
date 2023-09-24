91c1f474422ec0195fa73830e72933bab7e46a92
kosatos <kosatosfront@yandex.ru>
2023-09-25 02:08:01 +0300

    add hero section on home page

---
+++ project/assets/app/src/js/pages/home/swipers.js
@@ -0,0 +1,58 @@
+import Swiper from 'swiper/bundle';
+
+export default function swipers() {
+    // hero background
+    if (document.querySelector('.hero__bg') && document.querySelector('.hero-thumbs__swiper')) {
+        const currentFraction = document.querySelector('.hero-thumbs__fraction_current'),
+            totalFraction =  document.querySelector('.hero-thumbs__fraction_total')
+        // hero thumbs
+        new Swiper('.hero-thumbs__swiper', {
+            slidesPerView: 1,
+            slideActiveClass: 'hero-thumbs__slide_active',
+            loop: true,
+            speed: 1000,
+            effect: 'fade',
+            autoplay: {
+                delay: 7500,
+            },
+            allowTouchMove: false,
+            pagination: {
+                el: '.hero-thumbs__pagination',
+                type: 'progressbar',
+            },
+            breakpoints: {
+                1024: {
+                    pagination: {
+                        type: 'bullets',
+                        bulletClass: 'hero-thumbs__bullet',
+                        bulletActiveClass: 'hero-thumbs__bullet_active'
+                    },
+                }
+            },
+
+            on: {
+                realIndexChange: function (evt) {
+                    currentFraction.textContent = evt.realIndex + 1 < 10 ? `0${evt.realIndex + 1}` : evt.realIndex + 1
+                },
+                init: function (evt) {
+                    currentFraction.textContent = evt.realIndex + 1 < 10 ? `0${evt.realIndex + 1}` : evt.realIndex + 1
+                    totalFraction.textContent = evt.slides.length < 10 ? `0${evt.slides.length}` : evt.slides.length
+                },
+                breakpoint: function (evt) {
+                    evt.pagination.init()
+                },
+            }
+        })
+
+        new Swiper('.hero__bg', {
+            slidesPerView: 1,
+            loop: true,
+            speed: 1000,
+            autoplay: {
+                delay: 7500,
+            },
+            effect: "fade",
+            allowTouchMove: false
+        })
+    }
+}
