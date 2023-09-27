068df1a3f8cc6fd8a3c9924dcf4720bca1609d00
kosatos <kosatosfront@yandex.ru>
2023-09-28 01:38:44 +0300

    add review section

---
+++ project/assets/app/src/js/base/swipers.js
@@ -0,0 +1,14 @@
+import Swiper from 'swiper/bundle';
+
+export default function swipers() {
+    if (document.querySelector('.reviews__swiper')) {
+        new Swiper('.reviews__swiper', {
+            slidesPerView: 1,
+            speed: 1000,
+            navigation: {
+                nextEl: '.reviews__controls .swiper-button_next',
+                prevEl: '.reviews__controls .swiper-button_next'
+            }
+        })
+    }
+}
