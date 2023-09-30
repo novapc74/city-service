6d2ea63f7b6ee72a1029b21282d419328965afda
novapc74 <novapc@ukr.net>
2023-09-30 15:12:03 +0300

    refactor workCategory crud

--- project/src/Controller/Admin/WorkCategoryCrudController.php
+++ project/src/Controller/Admin/WorkCategoryCrudController.php
@@ -3,25 +3,36 @@
 namespace App\Controller\Admin;
 
 use App\Entity\WorkCategory;
-use App\Form\Admin\CompletedWorkType;
 use App\Form\Admin\GalleryType;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class WorkCategoryCrudController extends AbstractCrudController
 {
+    public function __construct(private readonly EntityManagerInterface $entityManager,
+                                private readonly AdminUrlGenerator      $adminUrlGenerator,
+                                private readonly RequestStack           $requestStack)
+    {
+    }
+
     public static function getEntityFqcn(): string
     {
         return WorkCategory::class;
@@ -34,6 +45,78 @@ class WorkCategoryCrudController extends AbstractCrudController
             ->setPageTitle('edit', 'Редактировать категорию')
             ->setPageTitle('new', 'Создать категорию')
             ->setEntityLabelInPlural('Категории работ');
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $addCompletedWork = Action::new('add completed work', 'Добавить работу')
+            ->displayAsLink()
+            ->displayIf(fn(WorkCategory $workCategory) => !$workCategory->getCategory())
+            ->linkToCrudAction('addCompletedWork');
+
+        $showCompletedWorks = Action::new('show completed works', 'Показать работы')
+            ->displayAsLink()
+            ->displayIf(fn(WorkCategory $workCategory) => $workCategory->getCompletedWorks()->count())
+            ->linkToCrudAction('showCompletedWorks');
+
+        $comeBack = Action::new('come back to parent category', 'Вернуться')
+            ->displayAsLink()
+            ->displayIf(fn(WorkCategory $category) => $category->getCategory())
+            ->linkToCrudAction('comeBackToParentCategory');
+
+        return parent::configureActions($actions)
+            ->add('index', $addCompletedWork)
+            ->add('index', $showCompletedWorks)
+            ->add('index', $comeBack);
+    }
+
+    public function comeBackToParentCategory(AdminContext $context): RedirectResponse
+    {
+        /**@var WorkCategory $currentCategory */
+        $currentCategory = $context->getEntity()->getInstance();
+
+        $url = $this->adminUrlGenerator->unsetAll()
+            ->setController(WorkCategoryCrudController::class)
+            ->setAction('index')
+            ->set('entity_id', $currentCategory->getCategory()->getId())
+            ->generateUrl();
+
+        return $this->redirect($url);
+    }
+
+    public function showCompletedWorks(AdminContext $context): RedirectResponse
+    {
+        /**@var WorkCategory $currentCategory */
+        $currentCategory = $context->getEntity()->getInstance();
+        $idCompletedWoks = $currentCategory->getCompletedWorks()->map(fn(WorkCategory $completedWork) => $completedWork->getId())->toArray();
+
+        $url = $this->adminUrlGenerator->unsetAll()
+            ->setController(WorkCategoryCrudController::class)
+            ->setAction('index')
+            ->set('entity_id', implode(',', $idCompletedWoks))
+            ->generateUrl();
+
+        return $this->redirect($url);
+    }
+
+    public function addCompletedWork(AdminContext $context): RedirectResponse
+    {
+        $newCompletedWork = (new WorkCategory())->setTitle('');
+
+        /**@var WorkCategory $currentCategory */
+        $currentCategory = $context->getEntity()->getInstance();
+        $currentCategory->addCompletedWork($newCompletedWork);
+
+        $this->entityManager->persist($newCompletedWork);
+        $this->entityManager->flush();
+
+        $url = $this->adminUrlGenerator->unsetAll()
+            ->setController(WorkCategoryCrudController::class)
+            ->setAction('edit')
+            ->setEntityId($newCompletedWork->getId())
+            ->generateUrl();
+
+        return $this->redirect($url);
     }
 
     public function configureFields(string $pageName): iterable
@@ -60,17 +143,27 @@ class WorkCategoryCrudController extends AbstractCrudController
                 ->renderExpanded()
                 ->onlyOnForms()
             ,
-            FormField::addTab('Выполненные работы'),
-            CollectionField::new('completedWorks', false)
-                ->setEntryType(CompletedWorkType::class)
-                ->setFormTypeOptions([
-                    'by_reference' => false,
-                    'error_bubbling' => false,
-                ])
-                ->renderExpanded()
-                ->setTemplatePath('admin/crud/assoc_description.html.twig')
-            ,
         ];
+    }
+
+    protected function getRedirectResponseAfterSave(AdminContext $context, string $action): RedirectResponse
+    {
+        /**@var WorkCategory $category */
+        $category = $context->getEntity()->getInstance();
+
+        if ($parentCategory = $category->getCategory()) {
+            $idCategoryCollection = $parentCategory->getCompletedWorks()->map(fn(WorkCategory $completedWork) => $completedWork->getId())->toArray();
+
+            $url = $this->adminUrlGenerator->unsetAll()
+                ->setController(WorkCategoryCrudController::class)
+                ->setAction('index')
+                ->set('entity_id', implode(',', $idCategoryCollection))
+                ->generateUrl();
+
+            return $this->redirect($url);
+        }
+
+        return parent::getRedirectResponseAfterSave($context, $action);
     }
 
     /**
@@ -80,6 +173,15 @@ class WorkCategoryCrudController extends AbstractCrudController
     public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
     {
         $repository = $this->container->get(EntityRepository::class);
+        $query = $this->requestStack->getCurrentRequest()->query;
+
+        if ($query->has('entity_id')) {
+            $idCollection = $query->get('entity_id');
+
+            return $repository->createQueryBuilder($searchDto, $entityDto, $fields, $filters)
+                ->andWhere('entity IN (:val)')
+                ->setParameter('val', explode(',', $idCollection));
+        }
 
         return $repository->createQueryBuilder($searchDto, $entityDto, $fields, $filters)
             ->andWhere('entity.category is null');
